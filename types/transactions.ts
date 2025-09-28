5326583cf5d5d182ea0d586658463c09a1ac9b5b
Samuel Calistus <89456479+deracali@users.noreply.github.com>
2025-09-28 17:12:52 +0100

    Update transactions.ts

--- types/transactions.ts
+++ types/transactions.ts
@@ -3,7 +3,7 @@ export type Transaction = {
   id: string
   usd_amount: string
   ngn_amount: string
-  status: 'pending' | 'failed' | 'completed'
+  status: 'pending' | 'failed' | 'success'
   created_at: string
   updated_at: string
   transaction_type: 'withdrawal' | 'deposit'
